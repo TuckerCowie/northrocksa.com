993b9049972fb3ba2e7bd11b1654835847b4f976
Tucker Cowie <TuckerCowie@gmail.com>
2016-01-08 17:58:24 -0600

    Use count to choose dynamic layout of homepage mosiac

--- wp-content/themes/sage/templates/mosiac-4.php
+++ wp-content/themes/sage/templates/mosiac-4.php
@@ -17,12 +17,23 @@
 			<i class="glyphicon glyphicon-chevron-right pull-right"></i>
 		</div>
 	</a>
-	<a class="nr_mosiac_flex-tile nr_mosiac_flex-tile--column nr_mosiac_flex-tile--vertical nr_card" href="<?= $mosiac[2]['link'] ?>">
-		<div class="nr_card_image nr_card_image--flex-grow" style="background-image: url(<?= $mosiac[2]['image'] ?>);">
-		</div>
-		<div class="nr_card_link">
-			<span class="text-primary"><?= $mosiac[2]['title'] ?></span>
-			<i class="glyphicon glyphicon-chevron-right pull-right"></i>
-		</div>
-	</a>
+	<div class="nr_mosiac_flex-tile--vertical">
+		<a class="nr_mosiac_flex-tile nr_mosiac_flex-tile--column nr_mosiac_flex-tile--horizontal-half nr_card" href="<?= $mosiac[2]['link'] ?>">
+			<div class="nr_card_image nr_card_image--flex-grow" style="background-image: url(<?= $mosiac[2]['image'] ?>);">
+			</div>
+			<div class="nr_card_link">
+				<span class="text-primary"><?= $mosiac[2]['title'] ?></span>
+				<i class="glyphicon glyphicon-chevron-right pull-right"></i>
+			</div>
+		</a>
+		<a class="nr_mosiac_flex-tile nr_mosiac_flex-tile--column nr_mosiac_flex-tile--horizontal-half nr_card" href="<?= $mosiac[3]['link'] ?>">
+			<div class="nr_card_image nr_card_image--flex-grow" style="background-image: url(<?= $mosiac[3]['image'] ?>);">
+			</div>
+			<div class="nr_card_link">
+				<span class="text-primary"><?= $mosiac[3]['title'] ?></span>
+				<i class="glyphicon glyphicon-chevron-right pull-right"></i>
+			</div>
+		</a>
+
+	</div>
 </div>
