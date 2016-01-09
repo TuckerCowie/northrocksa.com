4e085661a1dad6a1b8b005d48fb1a22969752928
Tucker Cowie <TuckerCowie@gmail.com>
2016-01-09 13:27:48 -0600

    Fix markup containing video thumbnail

--- wp-content/themes/sage/page-new-here.php
+++ wp-content/themes/sage/page-new-here.php
@@ -8,13 +8,18 @@
 				<?php if( have_rows('video') ):
     				while ( have_rows('video') ) : the_row();
     					$video = Vimeo\getVideo(get_sub_field('video_id')); ?>
-    					<a class="nr_card nr_card--video text-center" href="<?= $video->url; ?>">
-    						<img src="<?= $video->thumbnail_large; ?>">
-    						<span class="nr_card--video_badge">
-								<img width="50px" src="<?= get_template_directory_uri() . '/assets/images/play.svg' ?>">
-    						</span>
-        					<div class="nr_card_content text-primary"><h4><?php the_sub_field('title'); ?></h4></div>
-    					</a>
+						<a class="nr_card nr_card--video text-center na_video-box" href="https://player.vimeo.com/video/<?= get_sub_field('video_id'); ?>?portrait=0&badge=0&byline=0&autoplay=1&portrait=0&color=B23615" style="background-image: url(<?= $video->thumbnail_large; ?>);">
+							<div class="nr_card_image nr_card_image--16x9">
+								<div class="content">
+								</div>
+							</div>
+							<div class="nr_card--video_tray">
+								<div class="nr_card--video_badge">
+									<img width="50px" src="<?= get_template_directory_uri() . '/assets/images/play.svg' ?>">
+								</div>
+        						<div class="nr_card_content text-primary"><h4><?php the_sub_field('title'); ?></h4></div>
+							</div>
+						</a>
     				<?php endwhile; ?>
 					<hr>
 				<?php endif; ?>
