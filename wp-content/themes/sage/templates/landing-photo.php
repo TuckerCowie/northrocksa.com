38acb744187229bd274087f56d683a36c838d57f
Tucker Cowie <TuckerCowie@gmail.com>
2016-03-11 13:10:18 -0600

    Pin background photos to respective corners for better responsiveness

--- wp-content/themes/sage/templates/landing-photo.php
+++ wp-content/themes/sage/templates/landing-photo.php
@@ -1,4 +1,4 @@
-<div class="nr_parallax-bg" style="padding: 100px 0; margin: 0; background-image: url(<?= the_sub_field('section_photo'); ?>);>">
+<div class="nr_parallax-bg <?= get_sub_field('section_alignment') == 'right' ? 'nr_parallax-bg--right' : 'nr_parallax-bg--left'; ?>" style="padding: 100px 0; margin: 0; background-image: url(<?= the_sub_field('section_photo'); ?>);>">
 	<div class="container">
 		<div class="row">
 			<div class="col-lg-6 <?= get_sub_field('section_alignment') == 'right' ? 'col-md-offset-6' : ''; ?>">
