51af4c56ad7dbdf5b4a3d45262a96232c5c00f68
Tucker Cowie <TuckerCowie@gmail.com>
2016-03-18 17:30:27 -0500

    Add churchonline countdown

--- wp-content/themes/sage/templates/header.php
+++ wp-content/themes/sage/templates/header.php
@@ -1,3 +1,4 @@
+<?php get_template_part('templates/countdown'); ?>
 <header class="nr_navbar-container">
   <div class="container">
     <nav class="navbar navbar-default">
@@ -11,23 +12,9 @@
           <a class="navbar-brand" href="/">
             <img src="<?= get_template_directory_uri() . '/assets/images/logo-horizontal.png'; ?>" height="100%">
           </a>
-          <?php
-            function isSundayMorning() {
-              $dateTime = new DateTime('now', new DateTimeZone('America/Chicago'));
-              $dateTime->setTimestamp(time());
-              $currentHour = $dateTime->format('H');
-              return ($dateTime->format('l') == 'sunday') && ($currentHour > 7) && ($currentHour < 13);
-            }
-            if (isSundayMorning()):
-          ?>
-            <a href="http://northrocksa.churchonline.org/" target="_blank" class="btn btn-default navbar-btn hidden-xs hidden-sm" style="border-color:#dd2233;color:#dd2233;"> <span class="glyphicon glyphicon-facetime-video" aria-hidden="true"></span> Watch Live</a>
-          <?php endif; ?>
         </div>
         <div class="collapse navbar-collapse nr_navbar" id="top-nav">
-          <?php if (isSundayMorning()): ?>
-            <a href="#" class="btn btn-default navbar-btn btn-block visible-xs-block" style="border-color:#dd2233;color:#dd2233;"><span class="glyphicon glyphicon-facetime-video" aria-hidden="true"></span> Watch Live</a>
           <?php
-          endif;
           if (has_nav_menu('top_navigation')) :
             wp_nav_menu([
               'theme_location' => 'top_navigation',
